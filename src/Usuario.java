90f673449b5ddc04791b48d2e8c8fe8385e7edbe
IgnacioNovoa1 <ignacionovoa8@gmail.com>
2025-09-09 23:14:26 -0300

    Agrege comentarios al codigo

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,10 +1,12 @@
 public class Usuario {
     private String nombre;
 
+    //Creación de objeto
     public Usuario(String nombre) {
         this.nombre = nombre;
         
     }
+    //Metodo saludo
     public String Saludo() {
         return "Hola " + nombre;
     }
--- src/VentanaSaludo.java
+++ src/VentanaSaludo.java
@@ -3,11 +3,13 @@ import javax.swing.*;
 
 public class VentanaSaludo {
     public static void main(String[] args){
+        //Creacion de la ventana principal
         JFrame ventana = new JFrame("App de Saludo ICC490");
         ventana.setSize(420,220);
         ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         ventana.setLayout(null);
 
+        //Creacion de botones y etiqueta
         JTextField campoTexto = new JTextField();
         campoTexto.setBounds(20, 30, 260, 25);
 
@@ -20,6 +22,7 @@ public class VentanaSaludo {
         JLabel etiquetaSaludo = new JLabel("");
         etiquetaSaludo.setBounds(20, 110, 360, 25);
 
+        //Acción al presionar el boton Saludar
         botonSaludar.addActionListener(e -> {
             String nombre = campoTexto.getText();
             if (nombre == null || nombre.trim().isEmpty()){
@@ -31,12 +34,14 @@ public class VentanaSaludo {
 
             }
         });
+        //Acción al presionar el boton Limpiar
         botonLimpiar.addActionListener(e -> {
             campoTexto.setText("");
             etiquetaSaludo.setText("");
             campoTexto.requestFocus();
 
         });
+        //Permite utilizar el Enter
         campoTexto.addKeyListener(new KeyAdapter () {
             @Override
             public void keyPressed(KeyEvent e) {
@@ -46,6 +51,7 @@ public class VentanaSaludo {
             }
         });
 
+        //Agrega los botones y etiqueta a la ventana principal
         ventana.add(campoTexto);
         ventana.add(botonSaludar);
         ventana.add(botonLimpiar);
